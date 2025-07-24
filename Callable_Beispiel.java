3c6598809c68e267f0bc247c3c35e7774c230ade
Martin1345 <69191598+Martin1345@users.noreply.github.com>
2025-07-24 14:41:27 +0200

    Update Callable_Beispiel.java

--- Callable_Beispiel.java
+++ Callable_Beispiel.java
@@ -6,22 +6,21 @@ import java.util.concurrent.Future;// Importierung der Klasse Future
 public class Callable_Beispiel {// Klasse zur Demonstration der Verwendung von Callable
     public static void main(String[] args) {
         
-        ExecutorService executor = Executors.newSingleThreadExecutor();//Erstellung eines ThreadPool, 
-        //der nur einen Thread verwaltet
+        ExecutorService executor = Executors.newSingleThreadExecutor();//Erstellung eines Threadpools, 
+        //der nur einen Thread verwaltet.
 
-    
         Callable<Integer> aufgabe = () -> {// Definition der Aufgabe, 
             //die auf dem Thread ausgeführt wird
             int zahl = 21;// Zahl, mit der durch den Thread 
             //gearbeitet wird
             Thread.sleep(500); // Simulation einer Rechenzeit von 500 
             //Millisekunden durch den Thread
-            return zahl * 2;   // Durchführung der Aufgabe(verdoppeln der Zahl)
+            return zahl * 2;   // Durchführung der Aufgabe(Verdoppeln der Zahl)
             //und Rückgabe des Ergebnisses
         };
 
         try {
-            // Weitergabe der Aufgabe an den Executor der diese an den Thread weitergibt
+            // Weitergabe der Aufgabe an den Executor, der diese an den Thread weitergibt.
             Future<Integer> ergebnis = executor.submit(aufgabe);//Der Executor führt 
             //die Aufgabe asynchron aus und gibt ein Future-Objekt zurück
 
@@ -33,7 +32,7 @@ public class Callable_Beispiel {// Klasse zur Demonstration der Verwendung von C
            // einer Fehlermeldung, falls bei der Ausführung des Threads eine Exception auftritt
         } finally {
         
-            executor.shutdown();// Beenden des Executors und Freigabe der Ressourcen auch wenn 
+            executor.shutdown();// Beenden des Executors und Freigabe der Ressourcen, auch wenn 
             //eine Exception aufgetreten ist
         }
     }
