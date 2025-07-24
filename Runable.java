39cb4459e61a4641dd75701eb5d0c359d41a6f69
Martin1345 <69191598+Martin1345@users.noreply.github.com>
2025-07-24 14:16:39 +0200

    Update Runable.java

--- Runable.java
+++ Runable.java
@@ -1,12 +1,12 @@
 package com.multithreading.examples;
 
-public class Runable {//Klasse zur Demonstration von Runnable in Java
+public class Runable {//Klasse zur Demonstration von Runable in Java
 
     public static void main(String[] args) {
 
         Runnable aufgabe1 = () -> {//Erstellung eines ersten Runnable-Objektes
-            for (int i = 1; i <= 5; i++) {//Runabele führt eine Schleife von 1 bis 5 aus
-                System.out.println(" Thread 1 Schritt " + i);// Rückgabe des aktuellen schrittes von Thread 1
+            for (int i = 1; i <= 5; i++) {//Runnabele führt eine Schleife von 1 bis 5 aus
+                System.out.println(" Thread 1 Schritt " + i);// Rückgabe des aktuellen Schrittes von Thread 1
                
             }
         };
@@ -18,12 +18,12 @@ public class Runable {//Klasse zur Demonstration von Runnable in Java
             }
         };
 
-        Thread thread1 = new Thread(aufgabe1);// Erzeugung eines Threads mit der Aufgabe1
-        Thread thread2 = new Thread(aufgabe2);// Erzeugung eines Threads mit der Aufgabe2
+        Thread thread1 = new Thread(aufgabe1);// Erzeugung eines Threads mit der Aufgabe 1
+        Thread thread2 = new Thread(aufgabe2);// Erzeugung eines Threads mit der Aufgabe 2
 
         thread1.start();// Start des ersten Threads
         thread2.start();// Start des zweiten Threads
 
-        System.out.println(" Beide Threads wurden gestartet!");// Ausgabe an den Nutzuer, sobald beide Threads gestartet wurden
+        System.out.println(" Beide Threads wurden gestartet!");// Ausgabe an den Nutzer, sobald beide Threads gestartet wurden
     }
 }
