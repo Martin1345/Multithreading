f73817828136c188720a0b8bb24121a3e4cb74f2
Martin1345 <69191598+Martin1345@users.noreply.github.com>
2025-07-24 13:56:46 +0200

    Add files via upload

---
+++ SleepDemo.java
@@ -0,0 +1,16 @@
+public class SleepDemo {//Klassem zur Demonstration von Thread.sleep()
+    public static void main(String[] args) {
+        System.out.println(" Starte Countdown...");//Ausdgabe, dass der Countdown gestartet wurde
+
+        for (int i = 5; i >= 1; i--) {//Schleife, die die Sekunden bis zum Aufwachen des Threads zählt
+            System.out.println(+ i + " Sekunden verbleiben...");// Jede Sekunde wird die Anzahl verbleibender Sekunden bis zum Aufwachen des Threads ausgegeben
+
+            try {
+                Thread.sleep(1000); // Thread schläft für 1000 Millisekunden (1 Sekunde)
+            } catch (InterruptedException e) {
+                System.out.println("❗ Der Schlaf wurde unterbrochen.");// Falls der Thread während des Schlafens unterbrochen wird, wird eine Fehlermeldung ausgegeben
+            }
+        }
+
+        System.out.println(" Start erfolgt!");// Rückmeldung dass der Countdown beendet ist und der Start erfolgt ist
+    }}
