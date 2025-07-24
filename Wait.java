98c208ec1a55771d962cfdb0a249bfd3fcbbf231
Martin1345 <69191598+Martin1345@users.noreply.github.com>
2025-07-24 14:59:39 +0200

    Update Wait.java

--- Wait.java
+++ Wait.java
@@ -4,7 +4,8 @@ public class Wait {
     public static void main(String[] args) {
         Thread wartender_Thread = new Thread(() -> {
             synchronized (sperre) {// Snchroninsation mit sperre 
-                System.out.println(" Thread 1 wartet...");// Ausgabe, dass der wartende Thread ausführbar wäre
+                System.out.println(" Thread 1 wartet...");// Ausgabe, dass der wartende Thread 
+                //ausführbar wäre
                 try {
                     sperre.wait(); // Der Thread wartet, bis er durch den notify() 
                     //Aufruf eines anderen Threads benachrichtigt wird
@@ -12,24 +13,27 @@ public class Wait {
                     System.out.println(" Thread wurde unterbrochen!");// Sollte der Thread unterbochen werden,
                     //so wird eine Fehlermeldung ausgegeben
                 }
-                System.out.println(" Thread 1 wurde benachrichtigt und macht weiter!");// Ausgabe an den Nutzer, dass Notify 
-                //aufgerufen wurde und der wartende Thread weieterarbeiten kann
+                System.out.println(" Thread 1 wurde benachrichtigt und macht weiter!");// Ausgabe 
+                //an den Nutzer, dass Notify aufgerufen wurde und der wartende Thread weieterarbeiten kann
                 
             }
         });
 
         Thread benachrichtigerThread = new Thread(() -> {
             try {
-                Thread.sleep(2000); //Thread 2 pausiert für 2 Sekunden, um eine verzögerung zu simmulieren, erst danach wird der
-                //wartende Thread benachrichtigt
+                Thread.sleep(2000); //Thread 2 pausiert für 2 Sekunden, um eine verzögerung zu simmulieren,
+                //erst danach wird der wartende Thread benachrichtigt
             } catch (InterruptedException e) {
-                e.printStackTrace();// Falls der Thread unterbrochen wird, wird eine Fehlermeldung ausgegeben
+                e.printStackTrace();// Falls der Thread unterbrochen wird, 
+                //wird eine Fehlermeldung ausgegeben
             }
 
             synchronized (sperre) {
-                System.out.println(" Thread 2 benachrichtigt...");// Ausgabe an den Nutzer, dass der benachrichtigende 
+                System.out.println(" Thread 2 benachrichtigt...");// Ausgabe an den Nutzer, 
+                //dass der benachrichtigende 
                 //Thread nun Notify aufruft
-                sperre.notify(); // Aufruf von notify() auf dem sperre Objekt, um den wartenden Thread zu benachrichtigen
+                sperre.notify(); // Aufruf von notify() auf dem sperre Objekt, 
+                //um den wartenden Thread zu benachrichtigen
             }
         });
 
